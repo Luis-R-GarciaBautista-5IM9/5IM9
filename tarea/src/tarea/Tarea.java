69540d850971aa84305b1681e0f7c1c457734f32
Luis-R-GarciaBautista-5IM9 <42345776+Luis-R-GarciaBautista-5IM9@users.noreply.github.com>
2018-08-20 20:58:49 -0500

    Add files via upload
    Tarea 1

---
+++ tarea/src/tarea/Tarea.java
@@ -0,0 +1,17 @@
+
+package tarea;
+
+/**
+ *
+ * @author Raul Garcia Bautista
+ */
+public class Tarea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Buenos días gente bendecida");
+    }
+    
+}
